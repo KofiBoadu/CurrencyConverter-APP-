fa1c125140dd1f84744e0a80118a7b9e32ec190f
Daniel Boadu <mrboadu3@gmail.com>
2022-12-02 00:13:29 -0500

    added a tempreture api request

--- Currency converter/app/conversion.py
+++ Currency converter/app/conversion.py
@@ -1,16 +1,22 @@
 
 from flask import Flask, render_template,request,  url_for
 from currency import *
+from weather import *
 
 
 app= Flask(__name__)
 app.config.from_object(__name__)
 
 
+
 @app.route('/',methods= ['GET','POST'])
 def Converter():
     if request.method=="GET":
-        return render_template("convert.html")
+        currentWeather= Weather()
+        data=currentWeather.report()
+        return render_template("convert.html",data=data)
+
+        
 
     if request.method== "POST":
         amount=request.form['amount']
@@ -30,10 +36,18 @@ def Converter():
                 conVert=url.apiReport()["result"]
                 results.append(conVert)
         data= {'from':results[0], 'to':results[1],'amount':results[2],'convertion':results[3]}
-        
         return render_template("main.html",data=data)
-    
-    
+
+
+
+# @app.route('/temp',methods=["GET",'POST'])
+# def Temp():
+#     if request.method=="GET":
+#         currentWeather= Weather()
+#         temp=currentWeather.report()
+#         return render_template("templates.html",temp=temp) 
+
+
 
 
 if __name__=="__main__":
